85328c59c8da4b946e84d01e901cc7a8bcec6df7
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2021-03-15 16:54:05 -0600

    scripts

--- posts/2021-03-13-git-readme/git-readme_files/git-readme.R
+++
@@ -1,6 +0,0 @@
-# User Stats
-
-user_stats <- function(user){
-  pander::pander(glue::glue('<h2>User Stats</h2><a href="https://github.com/anuraghazra/github-readme-stats" align="center"><img style="max-width:100%;" height="160" align="center"
-  src="https://github-readme-stats.vercel.app/api/top-langs/?username={user}&layout=compact&theme=gruvbox" /></a> \n\n\n'))
-}
